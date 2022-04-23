16af3a4293dbf3a13970bbc7822fc7a7daa13d63
Kirefel <kirefelan@gmail.com>
2022-04-23 17:59:27 +1000

    Localisation support + teleporters

--- Randomiser/Pickup Actions/RandomiserAction.cs
+++ Randomiser/Pickup Actions/RandomiserAction.cs
@@ -1,5 +1,7 @@
 ﻿using Game;
+using OriDeModLoader;
 using System;
+using System.Collections.Generic;
 
 namespace Randomiser
 {
@@ -50,6 +52,7 @@ namespace Randomiser
                 case "AC": return HandleAC();
                 case "KS": return HandleKS();
                 case "MS": return HandleMS();
+                case "TP": return HandleTP();
                 default:
                     return null;
             }
@@ -60,7 +63,7 @@ namespace Randomiser
             var skill = (AbilityType)Enum.Parse(typeof(AbilityType), parameters[0]);
             Characters.Sein.PlayerAbilities.SetAbility(skill, true);
 
-            return new RandomiserActionResult(skill.ToString(), '#');
+            return new RandomiserActionResult(Strings.Get("SKILL_" + skill), '#');
         }
 
         private RandomiserActionResult HandleEC()
@@ -75,7 +78,7 @@ namespace Randomiser
 
             UI.SeinUI.ShakeEnergyOrbBar();
 
-            return new RandomiserActionResult("Energy Cell");
+            return new RandomiserActionResult(Strings.Get("PICKUP_ENERGY_CELL"));
         }
 
         private RandomiserActionResult HandleSpiritLight()
@@ -87,7 +90,7 @@ namespace Randomiser
 
             Characters.Sein.Level.GainExperience(amount); // TODO multiplier
 
-            return new RandomiserActionResult($"{amount} experience");
+            return new RandomiserActionResult(Strings.Get("PICKUP_EXPERIENCE", amount));
         }
 
         private RandomiserActionResult HandleHC()
@@ -95,7 +98,7 @@ namespace Randomiser
             Characters.Sein.Mortality.Health.GainMaxHeartContainer();
             UI.SeinUI.ShakeHealthbar();
 
-            return new RandomiserActionResult("Health Cell");
+            return new RandomiserActionResult(Strings.Get("PICKUP_HEALTH_CELL"));
         }
 
         private RandomiserActionResult HandleAC()
@@ -104,7 +107,7 @@ namespace Randomiser
             Characters.Sein.Inventory.SkillPointsCollected++;
             UI.SeinUI.ShakeExperienceBar();
 
-            return new RandomiserActionResult("Ability Cell");
+            return new RandomiserActionResult(Strings.Get("PICKUP_ABILITY_CELL"));
         }
 
         private RandomiserActionResult HandleKS()
@@ -112,7 +115,7 @@ namespace Randomiser
             Characters.Sein.Inventory.CollectKeystones(1);
             UI.SeinUI.ShakeKeystones();
 
-            return new RandomiserActionResult("Keystone");
+            return new RandomiserActionResult(Strings.Get("PICKUP_KEYSTONE"));
         }
 
         private RandomiserActionResult HandleMS()
@@ -120,7 +123,41 @@ namespace Randomiser
             Characters.Sein.Inventory.MapStones++;
             UI.SeinUI.ShakeMapstones();
 
-            return new RandomiserActionResult("Mapstone Fragment");
+            return new RandomiserActionResult(Strings.Get("PICKUP_MAPSTONE_FRAGMENT"));
+        }
+
+        private static readonly Dictionary<string, string> tpMap = new Dictionary<string, string>
+        {
+            ["Forlorn"] = "forlorn",
+            ["Grotto"] = "moonGrotto",
+            ["Sorrow"] = "valleyOfTheWind",
+            ["Grove"] = "spiritTree",
+            ["Swamp"] = "swamp",
+            ["Valley"] = "sorrowPass",
+            ["Ginso"] = "ginsoTree",
+            ["Horu"] = "mountHoru",
+            ["Glades"] = "sunkenGlades",
+            ["Blackroot"] = "mangroveFalls"
+        };
+        private static readonly Dictionary<string, string> areaShortNameMap = new Dictionary<string, string>
+        {
+            ["Forlorn"] = "AREA_FORLORN_RUINS",
+            ["Grotto"] = "AREA_MOON_GROTTO",
+            ["Sorrow"] = "AREA_SORROW_PASS",
+            ["Grove"] = "AREA_HOLLOW_GROVE",
+            ["Swamp"] = "AREA_THORNFELT_SWAMP",
+            ["Valley"] = "AREA_VALLEY_OF_THE_WIND",
+            ["Ginso"] = "AREA_GINSO_TREE",
+            ["Horu"] = "AREA_MOUNT_HORU",
+            ["Glades"] = "AREA_SUNKEN_GLADES",
+            ["Blackroot"] = "AREA_BLACKROOT_BURROWS"
+        };
+        private RandomiserActionResult HandleTP()
+        {
+            TeleporterController.Activate(tpMap[parameters[0]]);
+
+            string areaNameKey = areaShortNameMap[parameters[0]];
+            return new RandomiserActionResult(Strings.Get("TELEPORTER_ACTIVATED", Strings.Get(areaNameKey)));
         }
     }
 }
