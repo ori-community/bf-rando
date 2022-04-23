4f3662c36e521c621426825ec834d8bea37f6cdf
Kirefel <kirefelan@gmail.com>
2022-04-23 19:02:28 +1000

    Extra double jump

---
+++ Randomiser/Bonus/ExtraJumpsPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace Randomiser
+{
+    [HarmonyPatch(typeof(SeinDoubleJump), nameof(SeinDoubleJump.ExtraJumpsAvailable))]
+    class ExtraJumpsPatch
+    {
+        static void Postfix(ref int __result)
+        {
+            __result += Randomiser.Inventory.extraJumps;
+        }
+    }
+}
