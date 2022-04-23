d2fc6cadc5456e7f2041caabeaa0d17d560d491f
Kirefel <kirefelan@gmail.com>
2022-04-23 13:43:38 +1000

    Fix github build

--- build.ps1
+++ build.ps1
@@ -5,7 +5,6 @@ rm -R .\release -ErrorAction SilentlyContinue
 mkdir .\release\Mods\Assets\OriDeRandomiser\
 
 cp .\build\OriDeRandomiser.dll .\release\Mods\
-cp .\build\OriDeUILib.dll .\release\Mods\
 cp .\Randomiser\Assets\LocationData.json .\release\Mods\Assets\OriDeRandomiser\
 cp .\convertseed.ps1 .\release\
 
