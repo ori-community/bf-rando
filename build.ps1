f56fb3b9707467b864aa73e5baa53ca9baaeb9ba
Kirefel <kirefelan@gmail.com>
2022-04-24 10:33:57 +1000

    Update build for localisation

--- build.ps1
+++ build.ps1
@@ -3,9 +3,11 @@ msbuild /t:Build /restore /p:Configuration=Release /p:OutDir=..\build
 rm -R .\release -ErrorAction SilentlyContinue
 
 mkdir .\release\Mods\Assets\OriDeRandomiser\
+mkdir .\release\Mods\strings\Randomiser\
 
 cp .\build\OriDeRandomiser.dll .\release\Mods\
 cp .\Randomiser\Assets\LocationData.json .\release\Mods\Assets\OriDeRandomiser\
+cp .\Randomiser\strings\* .\release\Mods\strings\Randomiser\
 cp .\convertseed.ps1 .\release\
 
 rm .\OriDeRandomiser.zip -ErrorAction SilentlyContinue
