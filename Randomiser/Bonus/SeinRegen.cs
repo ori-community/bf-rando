08483f015319215885e258672cb16b2fce894a8f
Kirefel <kirefelan@gmail.com>
2022-04-24 16:27:58 +1000

    Fix regen NRE on menu and regen while paused

--- Randomiser/Bonus/SeinRegen.cs
+++ Randomiser/Bonus/SeinRegen.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 namespace Randomiser
 {
-    public class SeinRegen : CustomSeinAbility
+    public class SeinRegen : CustomSeinAbility, ISuspendable
     {
         public override bool AllowAbility(SeinLogicCycle logicCycle) => true;
 
         private const float HealthPerSecond = 1 / 60f;
         private const float EnergyPerSecond = 1 / 60f;
 
+        public bool IsSuspended { get; set; }
+
+        public override void Awake()
+        {
+            base.Awake();
+            SuspensionManager.Register(this);
+        }
+
         public void Update()
         {
-            if (!Active)
+            // TODO put the custom abilities ON Sein so they won't execute if sein doesn't exist
+            if (!Active || !Characters.Sein || !Characters.Sein.Active)
                 return;
 
             Characters.Sein.Energy.Gain(Randomiser.Inventory.energyRegen * EnergyPerSecond * Time.deltaTime);
