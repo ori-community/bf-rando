66becfc66f8a35577261c1bec7cc9506b2ae037f
Kirefel <kirefelan@gmail.com>
2022-04-30 10:04:29 +1000

    Open keystone doors from both sides

---
+++ Randomiser/KeystoneDoorOpenFromBothSides.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace Randomiser
+{
+    [HarmonyPatch(typeof(DoorWithSlots), "get_SeinInRange")]
+    internal class KeystoneDoorOpenFromBothSides
+    {
+        private static bool Prefix(DoorWithSlots __instance, ref bool __result)
+        {
+            if (Randomiser.Seed.HasFlag(RandomiserFlags.ClosedDungeons))
+                return true;
+
+            __result = !__instance.OriHasTargets && __instance.DistanceToSein <= __instance.Radius;
+            return false;
+        }
+    }
+}
