75f1d04806d39bab7cf3ab7aae680e67d34a0ec3
Kirefel <kirefelan@gmail.com>
2023-02-11 19:39:32 +1100

    New format

--- build.ps1
+++ build.ps1
@@ -5,10 +5,11 @@ rm -R .\release -ErrorAction SilentlyContinue
 mkdir .\release\Mods\Assets\OriDeRandomiser\
 mkdir .\release\Mods\strings\Randomiser\
 
-cp .\build\OriDeRandomiser.dll .\release\Mods\
-cp .\Randomiser\Assets\* .\release\Mods\Assets\OriDeRandomiser\
-cp .\Randomiser\strings\* .\release\Mods\strings\Randomiser\
+cp .\build\OriDeRandomiser.dll .\release\
+cp .\Randomiser\Assets\* .\release\assets\
+cp .\Randomiser\strings\* .\release\strings\
 cp .\convertseed.ps1 .\release\
+cp .\mod.json .\release\
 
 rm .\OriDeRandomiser.zip -ErrorAction SilentlyContinue
 powershell Compress-Archive .\release\* .\OriDeRandomiser.zip
