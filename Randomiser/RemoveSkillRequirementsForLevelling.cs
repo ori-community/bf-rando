afcb727fcae31489542a4a7acf6c4421b1a1df83
Kirefel <kirefelan@gmail.com>
2023-03-19 14:04:56 +1100

    Remove skill requirements for levelling

---
+++ Randomiser/RemoveSkillRequirementsForLevelling.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace Randomiser
+{
+    [HarmonyPatch(typeof(SkillItem), nameof(SkillItem.Awake))]
+    internal static class RemoveSkillRequirementsForLevelling
+    {
+        private static void Postfix(SkillItem __instance)
+        {
+            __instance.RequiredAbilities.Clear();
+        }
+    }
+}
