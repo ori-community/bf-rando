15ed801ad24a2be41cb77b9714f3f720ec31ffe6
Kirefel <kirefelan@gmail.com>
2023-10-21 12:51:59 +1100

    Seperate rando save files

--- Randomiser/RandomiserMod.cs
+++ Randomiser/RandomiserMod.cs
@@ -31,7 +31,6 @@ public class RandomiserMod : BaseUnityPlugin
 
             harmony = new Harmony("com.ori.randomiser");
             harmony.PatchAll();
-            Logger.LogInfo("Patched harmony");
 
             Controllers.Add<RandomiserInventory>("b9d5727e-43ff-4a6c-a9d1-d51489b3733d", "Randomiser", mb => Randomiser.Inventory = mb as RandomiserInventory);
             Controllers.Add<RandomiserSeed>("df0ebc08-9469-4f58-9e10-f836115b797b", "Randomiser", mb => Randomiser.Seed = mb as RandomiserSeed);
@@ -39,29 +38,31 @@ public class RandomiserMod : BaseUnityPlugin
             Controllers.Add<RandomiserController>(null, "Randomiser");
             Controllers.Add<RandomiserMessageController>(null, "Randomiser", mb => Randomiser.Messages = mb as RandomiserMessageController);
             Controllers.Add<ArchipelagoController>(null, "Randomiser", mb => Randomiser.Archipelago = mb as ArchipelagoController);
-            Logger.LogInfo("Added controllers");
 
             CustomSeinAbilityManager.Add<SeinRegen>("cc6427ed-31bf-4a7a-b4c4-ad559000ced9");
             CustomSeinAbilityManager.Add<SeinHotColdSense>("eda5f877-889b-4be5-9ecb-1b499ce7053d");
             CustomSeinAbilityManager.Add<ExtraAirDash>("88f2d7ca-f017-43f1-8dbb-3ae2afcb4ff4");
             CustomSeinAbilityManager.Add<StatsController>("491cb0ce-cb60-4fe0-96c1-fa7012d6994b");
 
-            Logger.LogInfo("Added abilities");
             Hooks.Game.OnStartNewGame += () =>
             {
                 // This runs when ori rises from the ground after the prologue
                 Randomiser.Message(AreaMapObjectiveText.GetGoalMessage());
             };
 
-            Logger.LogInfo("Added hook");
             SceneBootstrap.RegisterHandler(RandomiserBootstrap.SetupBootstrap, "Randomiser");
             Settings.Bind(this);
-            //CustomMenuManager.RegisterOptionsScreen<RandomiserSettingsScreen>("Randomiser", 2);
 
             //if (ModLoader.GetMod("Discord") != null)
             //{
             //    IPC.SetValue("Discord.ActivityDetails", (Func<string>)DynamicText.BuildDiscordActivityStatus);
             //}
+
+            On.SaveGameController.GetSaveFilePath += (orig, self, slotIndex, backupIndex) =>
+            {
+                string filePath = orig(self, slotIndex, backupIndex);
+                return filePath.Substring(0, filePath.Length - 3) + "randosav";
+            };
 
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
         }
