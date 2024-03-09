a8b44b9fffd605438044b9d77e1261bce0e71438
Eiko <worst.mirari@gmail.com>
2024-03-09 14:16:12 -0800

    god fucking damnit

--- Randomiser/Pickups/PickupBasePatch.cs
+++ Randomiser/Pickups/PickupBasePatch.cs
@@ -11,7 +11,6 @@ namespace Randomiser.Pickups;
 [HarmonyPatch(typeof(PickupBase))]
 internal class PickupBasePatch
 {
-    protected static Dictionary<MoonGuid, Location> pbGuidToLoc = new Dictionary<MoonGuid, Location>();
     protected static HashSet<MoonGuid> shitList = new HashSet<MoonGuid>();
 
     [HarmonyPostfix]
@@ -19,25 +18,20 @@ internal class PickupBasePatch
     public static void FixedUpdatePatch(PickupBase __instance) {
         var selfGuid = __instance.GetGuid();
         if (shitList.Contains(selfGuid)) return;
-        if (!pbGuidToLoc.ContainsKey(selfGuid))
-        {
-            var guid = __instance.GetComponent<VisibleOnWorldMap>()?.MoonGuid;
-            if (guid is not null && Randomiser.Locations[guid] != null)
-                pbGuidToLoc[selfGuid] = Randomiser.Locations[guid];
-            else {
-                RandomiserMod.Logger.LogInfo($"Added {__instance.name} to the shitlist - {guid}");
-                shitList.Add(selfGuid);
-                return;
-            }
+        var guid = __instance.GetComponent<VisibleOnWorldMap>()?.MoonGuid;
+        if (guid is null) {
+            RandomiserMod.Logger.LogInfo($"Added {__instance.name} to the shitlist - {guid}");
+            shitList.Add(selfGuid);
+            return;
         }
-            var loc = pbGuidToLoc[selfGuid];
-            if(loc.HasBeenObtained() && !__instance.IsCollected) {
-                RandomiserMod.Logger.LogInfo($"name: {__instance.name} - loc {loc.name} collected already - removing.");
-                __instance.IsCollected = loc.HasBeenObtained();
-                if (__instance.DestroyOnCollect)
-                    InstantiateUtility.Destroy(__instance.DestroyTarget);
-                else
-                    __instance.DestroyTarget.SetActive(false);
+        var loc = Randomiser.Locations[guid];
+        if(loc != null && loc.HasBeenObtained() && !__instance.IsCollected) {
+            RandomiserMod.Logger.LogInfo($"name: {__instance.name} - loc {loc.name} collected already - removing.");
+            __instance.IsCollected = loc.HasBeenObtained();
+            if (__instance.DestroyOnCollect)
+                InstantiateUtility.Destroy(__instance.DestroyTarget);
+            else
+                __instance.DestroyTarget.SetActive(false);
         }
     }
 }
